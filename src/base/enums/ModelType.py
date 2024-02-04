20f8d328060f8d9518d2e0d04f80cb55238863f6
xxwonderwallxx <vika010501@gmail.com>
2024-02-04 02:28:44 +0200

    + created ML Helper class to exclude code duplications

---
+++ src/base/enums/ModelType.py
@@ -0,0 +1,12 @@
+#
+# src/classes/MoodState.py
+#
+# MoodState enum used to give a characteristic grade about cryptocurrency news
+#
+
+
+from enum import Enum
+
+
+class ModelType(Enum):
+    Sequential = 'Sequential'
